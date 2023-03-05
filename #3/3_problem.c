5404daef4c9994de49dbf341abd487c13dbc0171
MM1032003 <t.hacking88@gmail.com>
2023-03-06 01:01:56 +0200

    Third problem commit

---
+++ #3/3_problem.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+TASK:
+    Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+Examples:
+    2 -> 3 (1 + 2)
+    8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+*/
+
+
+int summation(int num) {
+  // your code here
+}
+
+
+int main() {
+    return;
+}
---
+++ #3/3_solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int summation(int num) {
+  int result = 0;
+  for (int i = 1; i <= num; i++) {
+    result += i;
+  }
+  return result;
+}
+
+int main() {
+    return;
+}
